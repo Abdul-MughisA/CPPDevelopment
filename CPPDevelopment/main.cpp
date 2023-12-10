511128e366da075cfb616b356cd1d88be3ee5995
A'yaan Abdul-Mughis <ayaanamughis@outlook.com>
2023-12-10 14:32:07 +0000

    Initial Commit

---
+++ CPPDevelopment/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CPPDevelopment
+//
+//  Created by A'yaan Abdul-Mughis on 10/12/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
